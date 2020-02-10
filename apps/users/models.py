9cb84cb4274496c7e525bf61283c151a5a7b2013
Luis Mendez <mendezgla.56@gmail.com>
2020-02-09 23:05:25 -0300

    debugging

--- apps/users/models.py
+++ apps/users/models.py
@@ -19,7 +19,7 @@ class Dedication(models.Model):
 class Gronner(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     extract = models.TextField(default='', blank=True, null=True)
-    #dedication = models.ForeignKey(Dedication, default='0', on_delete=models.SET_DEFAULT, blank=True, null=True)
+    dedication = models.ForeignKey(Dedication, on_delete=models.CASCADE, blank=True, null=True)
     points = models.IntegerField(default=0)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     birth = models.DateField()
