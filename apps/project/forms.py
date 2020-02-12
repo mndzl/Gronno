e2c840eaacc6eabc749e99980cb10d2313ccd8fc
Luis Mendez <mendezgla.56@gmail.com>
2020-02-12 13:21:08 -0300

    publication info min-length form

--- apps/project/forms.py
+++ apps/project/forms.py
@@ -4,7 +4,9 @@ from .models import Project, Category
 class CreateProject(forms.ModelForm):
     category = forms.ModelChoiceField(Category.objects.all(), required=True, label='Categoría', empty_label="Selecciona una categoría")
     title = forms.CharField(required=True, max_length=40, label='Título',widget=forms.TextInput(attrs={'placeholder':'Nombre del proyecto'}), error_messages={'unique': 'Ya existe un proyecto con este nombre.'})
-    description = forms.CharField(max_length=10000, 
+    description = forms.CharField(
+                                min_length=120,
+                                max_length=10000,
                                 label='Descripcion', 
                                 widget=forms.Textarea(attrs={'placeholder':'Descripción del proyecto: muéstranos como lo pensaste, como te organizaste, y que cosas aprendiste de él'}), 
                                 required=True)
