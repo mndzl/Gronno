fffe2acc53025e028fb0d7c95556ad495d162af7
Luis Mendez <mendezgla.56@gmail.com>
2020-02-18 16:55:15 -0300

    change notification order

--- apps/home/views.py
+++ apps/home/views.py
@@ -50,7 +50,7 @@ class homepage(LoginRequiredMixin, ListView):
         context["medals"] = medals
         context["projects"] = zip(self.object_list,comments, medals)
         context["personal_projects"] = user.project_set.filter(is_active=True).order_by('-date_posted')
-        context["notifications"] = Notification.objects.filter(user=user)
+        context["notifications"] = Notification.objects.filter(user=user).order_by('-date_created')
 
         return context
 
