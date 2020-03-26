e8d3f051adfda7828740d658aa774d626ed8b9c1
Luis Mendez <mendezgla.56@gmail.com>
2020-03-26 01:15:46 -0300

    Commenting with ajax

--- static/js/project_detail.js
+++ static/js/project_detail.js
@@ -29,12 +29,43 @@ $('.medal').click(function(e){
         $(this).css("transform","" );
     }
 
-
     if($(this).attr('style')=='opacity:1'){
         $(this).attr('style', 'opacity:.3');
     }else{
         $('.medal').attr('style', 'opacity:.3')
         $(this).attr('style', 'opacity:1');
     }
-})
+});
 
+$("#new_comment").submit(function(e){
+    e.stopImmediatePropagation();
+    e.preventDefault();
+
+    var name = $("#name").text();
+    var dedication = $("#dedication").text();
+    var urlusr = $("#usrlink").attr("href");
+    var image = $("#usrimg").attr("src")
+
+    $.post(window.location.href, $(this).serialize())
+    .done(function(){
+        text = $("#comment_text").val();
+        $("#comment_text").val("");
+        $(".comments").prepend(`
+            <div class="comment" style="border:1px solid #a8a8a8">
+            <div class="head">
+                <a href="${urlusr}" class="link"></a>
+                <div class="comment_user_image">
+                    <img class="user_img" src="${image}">
+                </div>
+                <div class="info_com">
+                    <span class="comment_date">Justo ahora</span>
+                    <div class="comment_user_name">${name}</div>
+                    <div class="comment_user_dedication">${dedication}</div>
+                </div>
+            </div>
+            <div class="comment_content">${text}</div>            
+        </div>`
+        );
+    });
+
+});
