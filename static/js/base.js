f45b93c0c0092c0913ef54cf41922e31cff71254
Luis Mendez <mendezgla.56@gmail.com>
2020-09-23 18:17:19 -0300

    Notif preview hidden on phone

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,13 @@
 $('.notifications').hover(function(){
-    $('.notifications-nav-box').toggleClass('display');
-    $('.notifications-count').remove();
-        $.ajax({
-            type:'GET',
-            url:'/see_notifications/',
-            data:{},    
-        });
+    if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        $('.notifications-nav-box').toggleClass('display');
+        $('.notifications-count').remove();
+            $.ajax({
+                type:'GET',
+                url:'/see_notifications/',
+                data:{},    
+            });
+    }
 });
 
 $('.notifications').click(function(){
